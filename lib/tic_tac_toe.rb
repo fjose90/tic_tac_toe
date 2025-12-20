8820c4f1dca11981c18d5e779f37e7dfe8ce1289
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-20 17:50:59 -0300

    feat: adiciona testes para troca de jogador e verificação de vitória no jogo

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,37 +1,20 @@
 # frozen_string_literal: true
 
-module TicTacToe
-  WINNING_LINES = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-  ]
+require_relative "tic_tac_toe/computer_player"
+require_relative "tic_tac_toe/game"
+require_relative "tic_tac_toe/human_player"
+require_relative "tic_tac_toe/player"
 
-  class Game
-    attr_reader :board
+TicTacToe::Game.new(
+  TicTacToe::ComputerPlayer,
+  TicTacToe::ComputerPlayer,
+).play
 
-    def initialize(_, _)
-      @board = Array.new(10)
-    end
+puts
 
-    def free_positions
-      (1..9).select { |pos| @board[pos].nil? }
-    end
+players_with_human = [
+  TicTacToe::HumanPlayer,
+  TicTacToe::ComputerPlayer,
+].shuffle
 
-    def
-  end
-
-  class Player
-  end
-
-  class HumanPlayer
-  end
-
-  class ComputerPlayer
-  end
-end
+TicTacToe::Game.new(*players_with_human).play
--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -24,4 +24,46 @@ RSpec.describe(TicTacToe::Game) do
 
     expect(game.board[1]).to(eq("X"))
   end
+
+  it "switches current player" do
+    first_player = game.current_player_id
+
+    game.switch_players!
+
+    expect(game.current_player_id).not_to(eq(first_player))
+  end
+
+  describe "#player_has_won?" do
+    it "returns true when a player completes a winning line" do
+      game.board[1] = "X"
+      game.board[2] = "X"
+      game.board[3] = "X"
+
+      player = double(marker: "X")
+
+      expect(game.player_has_won?(player)).to(be(true))
+    end
+  end
+
+  describe "#board_full?" do
+    it "returns true when there are no free positions" do
+      (1..9).each { |pos| game.board[pos] = "X" }
+
+      expect(game.board_full?).to(be(true))
+    end
+  end
+
+  describe "#current_player" do
+    let(:player_class) do
+      Class.new do
+        def initialize(*); end
+      end
+    end
+
+    it "returns the current player based on current_player_id" do
+      game = described_class.new(player_class, player_class)
+
+      expect(game.current_player).to(be_a(player_class))
+    end
+  end
 end
