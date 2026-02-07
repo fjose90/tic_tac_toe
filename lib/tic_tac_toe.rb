016ef860b6421a3f5a4155b8a5d74f585f4e90cd
Fernando José <fernando.tecnicoinfo7@gmail.com>
2026-02-07 16:41:25 -0300

    feat: encapsula a lógica de inicialização do jogo dentro da verificação de execução do arquivo

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,16 +5,18 @@ require_relative "tic_tac_toe/game"
 require_relative "tic_tac_toe/human_player"
 require_relative "tic_tac_toe/player"
 
-TicTacToe::Game.new(
-  TicTacToe::ComputerPlayer,
-  TicTacToe::ComputerPlayer,
-).play
+if __FILE__ == $PROGRAM_NAME
+  TicTacToe::Game.new(
+    TicTacToe::ComputerPlayer,
+    TicTacToe::ComputerPlayer,
+  ).play
 
-puts
+  puts
 
-players_with_human = [
-  TicTacToe::HumanPlayer,
-  TicTacToe::ComputerPlayer,
-].shuffle
+  players_with_human = [
+    TicTacToe::HumanPlayer,
+    TicTacToe::ComputerPlayer,
+  ].shuffle
 
-TicTacToe::Game.new(*players_with_human).play
+  TicTacToe::Game.new(*players_with_human).play
+end
